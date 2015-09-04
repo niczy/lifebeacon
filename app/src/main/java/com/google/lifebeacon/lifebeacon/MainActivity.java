9b3d32eca8c39429c409b8aa2a7477571f18e448
Nicholas <nicholas.zhaoyu@gmail.com>
2015-09-04 00:11:14 -0700

    Send location

--- app/src/main/java/com/google/lifebeacon/lifebeacon/MainActivity.java
+++ app/src/main/java/com/google/lifebeacon/lifebeacon/MainActivity.java
@@ -1,37 +1,76 @@
 package com.google.lifebeacon.lifebeacon;
 
+import android.location.Location;
 import android.net.wifi.WifiConfiguration;
 import android.net.wifi.WifiManager;
+import android.os.Handler;
+import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.HurlStack;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
+
+    TextView indicatorView;
+    GoogleApiClient mGoogleApiClient;
+    Handler mHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        connect("zzz", "531531531");
-        sendLocation();
+        mHandler = new Handler(Looper.getMainLooper());
+        final TextView ssidView = (TextView) findViewById(R.id.ssid);
+        final TextView passwordView = (TextView) findViewById(R.id.password);
+        buildGoogleApiClient();
+        indicatorView = (TextView) findViewById(R.id.indicator);
+        findViewById(R.id.connect).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                connect(ssidView.getText().toString(), passwordView.getText().toString());
+                mHandler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        sendLocation();
+                    }
+                }, 3000L);
+            }
+        });
+
     }
 
     private void connect(String ssid, String key) {
+        WifiManager wifiManager = (WifiManager)getSystemService(WIFI_SERVICE);
+        if (!wifiManager.isWifiEnabled()) {
+            wifiManager.setWifiEnabled(true);
+        }
         WifiConfiguration wifiConfig = new WifiConfiguration();
 
         wifiConfig.SSID = String.format("\"%s\"", ssid);
         wifiConfig.preSharedKey = String.format("\"%s\"", key);
 
-        WifiManager wifiManager = (WifiManager)getSystemService(WIFI_SERVICE);
+
         //remember id
         int netId = wifiManager.addNetwork(wifiConfig);
         wifiManager.disconnect();
@@ -41,22 +80,51 @@ public class MainActivity extends AppCompatActivity {
 
     private void sendLocation() {
         // Instantiate the RequestQueue.
-        RequestQueue queue = Volley.newRequestQueue(this);
-        String url ="http://www.google.com";
+        RequestQueue queue = Volley.newRequestQueue(this, new HurlStack() {
+            @Override
+            protected HttpURLConnection createConnection(URL url) throws IOException {
+                HttpURLConnection connection = super.createConnection(url);
+                connection.setInstanceFollowRedirects(true);
+                return connection;
+            }
+        });
+        String url ="https://www.google.com";
 
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
+                        Location mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+                                mGoogleApiClient);
+                        if (mLastLocation != null) {
+                            indicatorView.setText("Latitude: " + String.valueOf(mLastLocation.getLatitude()
+                                    + "\nLongtitude: " + String.valueOf(mLastLocation.getLongitude())));
+                        }
+                        indicatorView.setText(indicatorView.getText() + "\nLocation sent! Saved!");
                         Log.i("main", response);
                     }
                 }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                Log.e("main", error.getStackTrace().toString());
+                Log.e("main", "error" + error.toString());
+                mHandler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        sendLocation();
+                    }
+                }, 3000L);
             }
         });
         queue.add(stringRequest);
+    }
+
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+        mGoogleApiClient.connect();
     }
 
     @Override
@@ -79,5 +147,33 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Log.e("main", "GoogleApi connected");
+        LocationRequest mLocationRequest = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setInterval(10 * 1000)        // 10 seconds, in milliseconds
+                .setFastestInterval(1 * 1000); // 1 second, in milliseconds
+
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.e("main", "GoogleApi connection failed" + connectionResult.toString());
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        indicatorView.setText("Latitude: " + String.valueOf(location.getLatitude()
+                + "\nLongtitude: " + String.valueOf(location.getLongitude())));
     }
 }
